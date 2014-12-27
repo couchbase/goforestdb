def9c2bb45d91b35895c054a6c8218343a1f6920
Marty Schoch <marty.schoch@gmail.com>
2014-12-27 13:14:26 -0800

    added unit test for iterators on snapshot

--- iterator_test.go
+++ iterator_test.go
@@ -267,3 +267,77 @@ func TestForestDBIteratorPrev(t *testing.T) {
 	}
 
 }
+
+func TestForestDBIteratorOnSnapshot(t *testing.T) {
+	defer os.RemoveAll("test")
+
+	dbfile, err := Open("test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dbfile.Close()
+
+	kvstore, err := dbfile.OpenKVStoreDefault(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer kvstore.Close()
+
+	// store a bunch of values to test the iterator
+
+	kvstore.SetKV([]byte("a"), []byte("vala"))
+	kvstore.SetKV([]byte("b"), []byte("valb"))
+	kvstore.SetKV([]byte("c"), []byte("valc"))
+	kvstore.SetKV([]byte("d"), []byte("vald"))
+	kvstore.SetKV([]byte("e"), []byte("vale"))
+	kvstore.SetKV([]byte("f"), []byte("valf"))
+	kvstore.SetKV([]byte("g"), []byte("valg"))
+	kvstore.SetKV([]byte("h"), []byte("valh"))
+	kvstore.SetKV([]byte("i"), []byte("vali"))
+	kvstore.SetKV([]byte("j"), []byte("valj"))
+
+	dbfile.Commit(COMMIT_NORMAL)
+
+	kvinfo, err := kvstore.Info()
+	if err != nil {
+		t.Fatal(err)
+	}
+	snapshot, err := kvstore.SnapshotOpen(kvinfo.LastSeqNum())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	iter, err := snapshot.IteratorInit([]byte("c"), []byte("g"), ITR_NONE)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer iter.Close()
+
+	doc, err := iter.Get()
+	count := 0
+	var firstKey, lastKey []byte
+	for err == nil {
+		count++
+		if firstKey == nil {
+			firstKey = doc.Key()
+		}
+		lastKey = doc.Key()
+		err = iter.Next()
+		if err == nil {
+			doc, err = iter.Get()
+		}
+	}
+	if count != 5 {
+		t.Errorf("exptected to iterate 5, saw %d", count)
+	}
+	if string(firstKey) != "c" {
+		t.Errorf("expected fist key to be c, got %s", firstKey)
+	}
+	if string(lastKey) != "g" {
+		t.Errorf("expected lats key to be g, got %s", lastKey)
+	}
+	if err != RESULT_ITERATOR_FAIL {
+		t.Errorf("expected %#v, got %#v", RESULT_ITERATOR_FAIL, err)
+	}
+
+}
